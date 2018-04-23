8de924da2141c2f9bb30fc8cc91eaf281093e18f
DeepCFoxXx <learncode23@gmail.com>
2018-04-23 11:32:01 +0100

    cell added

--- src/components/GridRow.jsx
+++ src/components/GridRow.jsx
@@ -8,7 +8,22 @@ const GridRow = props => {
 
   const board = props.board;
 
+  const cell = (index) => (
+    <Cell
+      id={index}
+      player={props.player}
+      playSquare={playSquare}
+      gameWon={props.gameWon}
+      contents={board[index]}/>
+  )
 
+  return(
+    <tr>
+      {cell(props.firstCell)}
+      {cell(props.firstCell + 1)}
+      {cell(props.firstCell + 2)}
+    </tr>
+  )
 }
 
 export default GridRow;
