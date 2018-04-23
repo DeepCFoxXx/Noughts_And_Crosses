910998a3ff792b324c08744820fb7d4ba1389a13
DeepCFoxXx <learncode23@gmail.com>
2018-04-23 11:58:01 +0100

    check rows for winner added

---
+++ src/components/Board.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Row from './GridRow.jsx'
+
+const Board = props => {
+
+  const checkRowsForWinner = function() {
+    const board = props.board;
+    if (((board[0] !== "") && (board[0] === board[1]) &&
+         (board[0] === board[2])) ||
+        ((board[3] !== "") &&(board[3] === board[4]) &&
+         (board[3] === board[5])) ||
+        ((board[6] !== "") &&(board[6] === board[7]) &&
+         (board[6] === board[8]))
+      ) {
+      return true
+    }
+    return false
+  }
+
+}
+
+export default Board;
