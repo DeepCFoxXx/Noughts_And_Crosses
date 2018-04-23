56e0defe216d3b54be9d1f821846bdd277be7da7
DeepCFoxXx <learncode23@gmail.com>
2018-04-23 11:12:09 +0100

    play square added

---
+++ src/components/GridRow.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const GridRow = props => {
+
+  const playSquare = function(cell) {
+    props.playSquare(cell)
+  }
+
+  const board = props.board;
+
+
+}
+
+export default GridRow;
