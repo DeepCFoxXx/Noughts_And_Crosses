10ecc7c317af2ced06f2714df6d61adeaf4ae9c6
DeepCFoxXx <learncode23@gmail.com>
2018-04-23 12:05:34 +0100

    check colums for winners added

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -17,6 +17,20 @@ const Board = props => {
     return false
   }
 
+  const checkColumnsForWinner = function() {
+    const board = props.board;
+    if (((board[0] !== "") && (board[0] === board[3]) &&
+         (board[0] === board[6])) ||
+        ((board[1] !== "") &&(board[1] === board[4]) &&
+         (board[1] === board[7])) ||
+        ((board[5] !== "") &&(board[2] === board[5]) &&
+         (board[2] === board[8]))
+      ) {
+      return true;
+    }
+    return false;
+  }
+
 }
 
 export default Board;
